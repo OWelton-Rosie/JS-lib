2cd478091d908de35173d6a5d52ba1dc6cdf0b1c
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-02-24 09:40:21 +1300

    Update scroll.js

--- website/scroll.js
+++ website/scroll.js
@@ -1,12 +1,6 @@
-
-        document.querySelectorAll('nav a').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                const targetId = this.getAttribute('href').substring(1);
-                const targetElement = document.getElementById(targetId);
-                window.scrollTo({
-                    top: targetElement.offsetTop - 20,
-                    behavior: 'smooth'
-                });
-            });
-        });
+window.onload = function() {
+    const now = new Date();
+    const time = now.toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland", hour: "2-digit", minute: "2-digit" });
+    const date = now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland", year: "numeric", month: "long", day: "numeric" });
+    document.getElementById("timeDisplay").innerHTML = `<b>${time}</b>, ${date}`;
+};
